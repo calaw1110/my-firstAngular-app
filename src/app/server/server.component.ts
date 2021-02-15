3c80db02370abc41b1f9d9b0799dc46b06e83214
calaw <calaw1110@gmail.com>
2021-02-15 17:05:59 +0800

    component製作學習中

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,8 +2,14 @@ import { Component } from "@angular/core";
 
 @Component(
   {
-    selector:'app-server',
-    templateUrl:'./server.component.html'
+    selector: 'app-server',
+    templateUrl: './server.component.html'
   }
 )
-export class ServerComponent { }
+export class ServerComponent {
+  serverID: number = 1;
+  serverStatus: string = '不在線上';
+  getServerStatus() {
+    return this.serverStatus;
+  }
+}
